6f3d0d0e1c0b40731e6e1339dbc4de87324650a2
kim980onit <kimanikamau971@gmail.com>
2024-01-22 03:35:17 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
        try:
            print("{}".format(my_list[i]), end="")
            ret +=1
-        exept indexError:
+       except: indexError
             break
    print("")
    return (ret)
