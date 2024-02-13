5f2e07323128fdf2b4f4ff3a5869f96ec7e8f4b2
kim980onti <kimanikamau971@gmail.com>
2024-02-13 20:17:17 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-        for j in range(i + 1, 10):
-                    print("{:02}".format(i * 10 + j), end=', ' if (i < 8 or
-                                    (i == 8 and j < 9)) else '\n')
+    for j in range(i + 1, 10):
+        condition = (i < 8 or (i == 8 and j < 9))
+        print("{:02}".format(i * 10 + j), end=', ' if condition else '\n')
