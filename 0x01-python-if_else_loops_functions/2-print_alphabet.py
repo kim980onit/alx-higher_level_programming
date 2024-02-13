ece418aa0f415da29f75fd72fda248297c74d76a
kim980onti <kimanikamau971@gmail.com>
2024-02-13 13:59:14 +0300

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 ascii_a = ord('a')
 
-alphabet = ''.join(chr(ascii_a + i)alphabet = ''.join(chr(ascii_a + i) for i in range(26)
+alphabet =''.join(chr(ascii_a + i)alphabet =''.join(chr(ascii_a + i) for i in range(26)
     print("{}".format(alphabet))
