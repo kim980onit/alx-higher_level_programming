ef4c2e45fc9df3df7247afa92b8819177221540b
kim980onit <kimanikamau971@gmail.com>
2024-04-09 17:30:57 +0300

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const numOfArg = process.argv.length - 2;
 
 if (numOfArg === 0) {
-	console.log('No argument');
-}else if (numOfArg === 1){
-	console.log('Argument found');
-}else {
-	console.log('Argument found');
+  console.log('No argument');
+} else if (numOfArg === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
