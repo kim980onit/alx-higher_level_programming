62defef619e1e0e1b214543eb829af7c1dd20a86
kim980onit <kimanikamau971@gmail.com>
2024-04-18 17:45:48 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
+"""
+
 import sys
 import MySQLdb
 
