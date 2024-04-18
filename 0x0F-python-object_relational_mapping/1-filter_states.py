bfeef9c090d723e6342789bebd25fe4cdfb66ef3
kim980onit <kimanikamau971@gmail.com>
2024-04-19 02:04:39 +0300

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
 
 """
-A script that lists all states from the database hbtn_0e_0_usa
-starting with a capital letter N
-Username, password, and database names are given as user args
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
 """
 
 import sys
 import MySQLdb
 
-def main():
-    """
-    Main function to execute the script
-    """
-
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-
-    try:
-        db = MySQLdb.connect(user=sys.argv[1],
-                             password=sys.argv[2],
-                             database_name=sys.argv[3],
-                             host='localhost',
-                             port=3306)
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-        sys.exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
     cursor = db.cursor()
 
-    try:
-        cursor.execute("SELECT * FROM states \
-                        WHERE name LIKE BINARY 'N%' \
-                        ORDER BY id ASC")
-    except MySQLdb.Error as e:
-        print("Error executing SQL query:", e)
-        cursor.close()
-        db.close()
-        sys.exit(1)
+    cursor.execute("SELECT * FROM states\
+                    WHERE name LIKE BINARY 'N%'\
+                    ORDER BY id ASC")
 
     data = cursor.fetchall()
 
@@ -48,6 +29,3 @@ def main():
 
     cursor.close()
     db.close()
-
-if __name__ == '__main__':
-    main()
