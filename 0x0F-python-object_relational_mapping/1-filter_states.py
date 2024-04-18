91ffc6d1b74c5b110e0f5f031e9100c1e31eb899
kim980onit <kimanikamau971@gmail.com>
2024-04-18 18:57:38 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
+Script to list all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+
+Usage:
+    python3 1-filter_states.py <mysql_username> <mysql_password> <database_name>
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python3 1-filter_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database_name)
-    cursor = db.cursor()
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+        cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    states = cursor.fetchall()
+        states = cursor.fetchall()
 
-    for state in states:
-        print(state)
-    
-    cursor.close()
-    db.close()
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
