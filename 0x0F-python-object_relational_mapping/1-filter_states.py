77668d82444c7cf40774a2da77387f3c5ca24351
kim980onit <kimanikamau971@gmail.com>
2024-04-18 19:04:17 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-Script to list all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+Script to list all states with a name starting with N (upper N) from the
+database hbtn_0e_0_usa.
 
 Usage:
-    python3 1-filter_states.py <mysql_username> <mysql_password> <database_name>
+    python3 1-filter_states.py <mysql_username> <mysql_password>
+    <database_name>
 """
 
 import MySQLdb
@@ -11,7 +13,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python3 1-filter_states.py <mysql_username> <mysql_password> <database_name>")
+        print("Usage: python3 1-filter_states.py <mysql_username>
+                <mysql_password> <database_name>")
         sys.exit(1)
 
     username = sys.argv[1]
@@ -19,10 +22,12 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                              passwd=password, db=database_name)
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+                        id ASC")
 
         states = cursor.fetchall()
 
