895bb9ebd0573952240b37d70b1076d3f601a562
kim980onit <kimanikamau971@gmail.com>
2024-04-18 20:02:21 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,8 +21,8 @@ def main():
 
     try:
         db = MySQLdb.connect(user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3],
+                             password=sys.argv[2],
+                             database_name=sys.argv[3],
                              host='localhost',
                              port=3306)
     except MySQLdb.Error as e:
