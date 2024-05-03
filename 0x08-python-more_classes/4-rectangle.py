e189e3e9b14d1bc6790bef6e675a94254ab3cbb7
kim980onit <kimanikamau971@gmail.com>
2024-05-03 14:57:46 +0300

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -34,7 +34,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -64,4 +64,4 @@ class Rectangle:
 
     def __repr__(self):
         """Returns a string representation to create a new instance"""
-        return "Rectangle({},{})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
